6af943b5bf3d868fa808bf83bdf53f659dafdba6
Russi1555 <78427180+Russi1555@users.noreply.github.com>
2023-05-31 14:42:25 -0300

    Continuação T3

--- kojun.pl
+++ kojun.pl
@@ -14,10 +14,25 @@ vertical([Higher, Lower| Column]) :- % faz a verificação vertical
 different_val(cell(_, Val1), cell(_, Val2)) :- % as duas células devem ter valores diferentes
     Val1 \= Val2.
 
-kojun(Board) :-     
-    maplist(horizontal, Board),   % Faz as limitações dentro de cada linha
-    
-    transpose(Board, BoardCol),   % Pega as colunas
-    
-    maplist(vertical, BoardCol).  % Faz as limitações dentro de cada coluna
+adjacencias(Tab) :-
+    maplist(horizontal, Tab),   % Faz as limitações dentro de cada linha
+    transpose(Tab, TabColuna),   % Pega as colunas
+    maplist(vertical, TabColuna).  % Faz as limitações dentro de cada coluna
+
+list_regions(LCells, Regions) :-
+    findall(Region, member(cell(Region, _), LCells), RegionsList),
+    sort(RegionsList, Regions).
+
+verify_region(LCells, Region) :-
+    vals_in_region(LCells, Region, Vals),
+    length(Vals, Len), 
+    Vals ins 1..Len,    % Valores estão entre 1 e Len
+    all_distinct(Vals), % Todos os valores tem q ser diferentes
+    maplist(portray_clause, Vals).
+
+kojun(Tab) :-     
+    adjacencias(Tab),
+    append(Tab, Celulas),
+    list_regions(Celulas, Regioes),
+    print(Regioes).
    
