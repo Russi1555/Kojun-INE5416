8a3994367ebcafb5555f0a30b28672273128bc4e
Russi1555 <78427180+Russi1555@users.noreply.github.com>
2023-06-01 14:14:55 -0300

    Repensando totalmente o T3

--- kojun.pl
+++ kojun.pl
@@ -1,38 +1,21 @@
 :- use_module(library(clpfd)).
-horizontal([]).
-horizontal([_| []]).
-horizontal([Left, Right| Row]) :- % faz a verificação horizontal
-    different_val(Left, Right), % células adjacentes devem ter valores diferentes
-    horizontal([Right| Row]).
 
-vertical([]).
-vertical([_| []]).
-vertical([Higher, Lower| Column]) :- % faz a verificação vertical
-    different_val(Higher, Lower), % células adjacentes devem ter valores diferentes
-    vertical([Lower| Column]).
+%Pode ser lateral ou horizontal. É só transpor a matriz
+check_adjacencia([]).
+check_adjacencia([_|[]]).
+check_adjacencia([_-Left,Region-Right|Tail]) :-
+    %Valor a esquerda deve ser diferente do valor a direita.
+    %Com a matriz transposta é possível fazer a mesma verficação
+    %Para adjacencia vertical.
+    Left #\= Right,
+    %Recursividade até o final da linha/coluna
+    adjacencia([Region-Right|Tail]).
 
-different_val(cell(_, Val1), cell(_, Val2)) :- % as duas células devem ter valores diferentes
-    Val1 \= Val2.
-
-adjacencias(Tab) :-
-    maplist(horizontal, Tab),   % Faz as limitações dentro de cada linha
-    transpose(Tab, TabColuna),   % Pega as colunas
-    maplist(vertical, TabColuna).  % Faz as limitações dentro de cada coluna
-
-list_regions(LCells, Regions) :-
-    findall(Region, member(cell(Region, _), LCells), RegionsList),
-    sort(RegionsList, Regions).
-
-verify_region(LCells, Region) :-
-    vals_in_region(LCells, Region, Vals),
-    length(Vals, Len), 
-    Vals ins 1..Len,    % Valores estão entre 1 e Len
-    all_distinct(Vals), % Todos os valores tem q ser diferentes
-    maplist(portray_clause, Vals).
-
-kojun(Tab) :-     
-    adjacencias(Tab),
-    append(Tab, Celulas),
-    list_regions(Celulas, Regioes),
-    print(Regioes).
-   
+%Regra de valor inferior/superior
+check_regra_vertical([]).
+check_regra_vertical([_|[]]).
+check_regra_vertical([Regiao_superior-Valor_superior,Regiao_inferior-Valor_inferior|Tail]) :-
+    %Regiao_superior diferente de Regiao_inferior OU Valor_superior > Valor_Inferior
+    (Regiao_superior #\= Regiao_inferior) #\/ (Valor_superior #> Valor_inferior),
+    %Recursividade até o final da linha
+    check_regra_vertical([Regiao_inferior-Valor_inferior|Tail]).
