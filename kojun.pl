0dd69d9a978071fe81025f7de131d291fd041100
Russi1555 <78427180+Russi1555@users.noreply.github.com>
2023-06-14 11:30:56 -0300

    Comentado
    Terminar o tabuleiro(10) e refazer o tabuleiro(1) (ele não é o janko 01)

--- kojun.pl
+++ kojun.pl
@@ -9,6 +9,13 @@ tabuleiro(1, [[(00-_), (00-_), (01-_), (01-_), (02-_), (03-_), (04-_), (04-_)],
               [(12-_), (12-_), (12-_), (12-_), (16-_), (14-_), (14-3), (14-_)],
               [(17-_), (16-_), (16-5), (16-3), (16-_), (14-_), (18-_), (18-_)]]).
 
+tabuleiro(10, [[(00-5), (01-_), (01-2), (01-_), (02-2), (02-_), (02-3), (02-1), (03-3), (03-1)],
+               [(00-_), (00-4), (00-_), (01-1), (04-_), (04-5), (06-_), (06-5), (03-_), (06-4)],
+               [(07-7), (07-5), (00-1), (04-7), (04-_), (08-_), (09-3), (06-1), (06-3), (06-_)],
+               [(07-_), (07-4), (04-_), (04-_), (10-_), (08-_), (09-_), (09-_), (09-_), (11-3)],
+               [(07-2), (07-_), (07-3), (04-4), (10-_), (10-2), (12,_), (11-_), (11-4), (11-_)],
+               [(13-5), (13-_), (14-2), (14-_), (14-6), (10-2), (15-_), (15-_), (16-_), (16-_)],
+
 %Pode ser lateral ou horizontal. É só transpor a matriz
 check_adjacencia([]).
 check_adjacencia([_|[]]).
@@ -22,52 +29,61 @@ check_adjacencia([_-Left,Region-Right|Tail]) :-
 
 %Regra de valor inferior/superior
 check_regra_vertical([]).
-check_regra_vertical([_|[]]).
+check_regra_vertical([_|[]]).07
 check_regra_vertical([Regiao_superior-Valor_superior,Regiao_inferior-Valor_inferior|Tail]) :-
     %Regiao_superior diferente de Regiao_inferior OU Valor_superior > Valor_Inferior
     (Regiao_superior #\= Regiao_inferior) #\/ (Valor_superior #> Valor_inferior),
     %Recursividade até o final da linha
     check_regra_vertical([Regiao_inferior-Valor_inferior|Tail]).
 
-region_max(Board, Region, Max) :-
-    append(Board, FlatBoard), sort(FlatBoard, Sorted), group_pairs_by_key(Sorted, Groups), member(Region-Value, Groups), length(Value, Max).
+maximo_regiao(Tab, Regiao, Max) :-
+    append(Tab, Linha_unica), %Concatena todas as linhas do tabuleiro em uma única linha
+    sort(Linha_unica, Linha_ordenada), %Ordena em ordem crescente todos os itens da lista
+    group_pairs_by_key(Linha_ordenada, Grupos), %Agrupa os itens da lista ordenada de acordo com o valor da região
+    member(Regiao-Valor, Grupos), %Pega as tuplas região-valor de dada região
+    length(Valor, Max). %valor máximo possível da região é o tamanho da lista de tuplas.
 
-limits(_, []).
-limits(Board, [Region-Value|[]]) :-
-    region_max(Board, Region, Max),
-    Value in 1..Max.
-limits(Board, [Region-Value, Next|Tail]) :-
-    region_max(Board, Region, Max),
-    Value in 1..Max,
-    limits(Board, [Next|Tail]).
+%limits(_, []). %condição de parada da recursão (acho que é desnecessário)
+limites(Tab, [Regiao-Valor|[]]) :- %tratamento da última célula do tabuleiro
+    maximo_regiao(Tab, Regiao, Max),
+    Valor in 1..Max.
+limites(Tab, [Regiao-Valor, Next|Tail]) :- %recursividade para buscar os limites da célula
+    maximo_regiao(Tab, Regiao, Max), %chamamos maximo_regiao para encontrar o valor máximo da célula de acordo com a região
+    Valor in 1..Max, %estabelecemos que Valor deve estar entre 1 e Max
+    limites(Tab, [Next|Tail]). %chamada recursiva de limites.
 
-distinct(_-List) :- all_distinct(List).
-distinct_by_region(Board) :-
-    append(Board, FlatBoard),
-    sort(FlatBoard, Sorted),
-    group_pairs_by_key(Sorted, Groups),
-    maplist(distinct, Groups).
+distintos(_-List) :- all_distinct(List). %retorna verdadeiro se não há repetições na lista recebida
+check_regra_repetição(Tab) :- %distinct sendo chamado para verificar a regra 
+    append(Tab, Linha_unica), %Tabuleiro vira uma linha única ao inves de uma matriz
+    sort(Linha_unica, Linha_ordenada), %linha é ordenada em ordem crescente
+    group_pairs_by_key(Linha_ordenada, Grupos), % são separados por regiões
+    maplist(distintos, Grupos). %distintos é aplicado em todas as regiões
 
-value_only([], []).
-value_only([H|T], [H2|T2]) :- pairs_values(H, H2), value_only(T, T2).
+get_valores([], []). %condição de parada da recursão
+get_valores([H|T], [H2|T2]) :-  %H = tupla atual, T = cauda da lista, H2 = lista de valores de retorno, T2 = Cauda da lista de retorno
+    pairs_values(H, H2), %coloca o valor de H em H2
+    get_valores(T, T2). %chamada recursiva de get_valores
 
-guess(Rows) :-
-    value_only(Rows, Values),
+chutes(Linhas) :-
+    get_valores(Linhas, Values), %pega a lista de valores
+    %aplica o predicado imbutido "label" na lista de valores. atribui uma solução válida para cada valor vazio da lista
+    %de acordo com as restrições estabelecidas anteriormente.
     maplist(label, Values).
 
-pretty(Rows) :- value_only(Rows, Values), maplist(portray_clause, Values).
+printa_tabuleiro(Linhas) :- 
+    get_valores(Linhas, Values), 
+    maplist(portray_clause, Values).
 
-kojun(Rows) :-
-    maplist(limits(Rows), Rows),
-    distinct_by_region(Rows),
-    maplist(check_adjacencia, Rows),
-    transpose(Rows, Columns),
-    maplist(check_adjacencia, Columns),
-    maplist(check_regra_vertical, Columns).
+kojun(Linhas) :-
+    maplist(limites(Linhas), Linhas),
+    check_regra_repetição(Linhas),
+    maplist(check_adjacencia, Linhas),
+    transpose(Linhas, Colunas),
+    maplist(check_adjacencia, Colunas),
+    maplist(check_regra_vertical, Colunas).
 
 resolve(N) :-
     tabuleiro(N,Tab),
     kojun(Tab),
-    guess(Tab),
-    pretty(Tab).
-
+    chutes(Tab),
+    printa_tabuleiro(Tab).
